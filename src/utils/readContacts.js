5b3e021197145be2c4f66065afea56faed58361b
denis-mahei <denmahei@gmail.com>
2025-06-22 22:32:14 +0300

    add fs.readFile

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB);
+    return JSON.parse(data.toString());
+  } catch (e) {
+    console.log('Error reading file:', e);
+  }
+};
