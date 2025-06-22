07caac1d3d5fb5be060a0c1f573eb16fa38cb963
denis-mahei <denmahei@gmail.com>
2025-06-22 22:48:10 +0300

    add generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,18 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const existingContacts = await readContacts();
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+    const updatedContacts = [...existingContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 generateContacts(5);
