cd727866e826b6a24e29becfd0151e3ccf7665f8
denis-mahei <denmahei@gmail.com>
2025-06-23 23:01:55 +0300

    done node-hw-01

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,12 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+
+  const rmLastContact = contacts.slice(0, -1);
+
+  return await writeContacts(rmLastContact);
+};
 
 removeLastContact();
